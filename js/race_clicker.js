da3267571427a39d290a038255f7e68ea21e0164
lyhe34 <luc.eymard@live.fr>
2023-05-12 12:22:39 +0200

    progress bar

--- js/race_clicker.js
+++ js/race_clicker.js
@@ -1,3 +1,40 @@
-function startRace() {
+var button;
+var buttonContent;
+var progressBar;
 
+var width = 0;
+var isRacing = false;
+var id;
+
+function startRace() {
+    if(!isRacing) {
+        isRacing = true;
+        
+        button = document.getElementById("race_clicker_button");
+        buttonContent = document.getElementById("rc_button_content");
+        progressBar = document.getElementById("bar");
+
+        // Save default button's css, to reset it when race ends.
+        var default_button_style = button.style;
+
+        id = setInterval(barProgression, 10);
+
+        button.onclick = "update()"
+    }
+}
+
+function update() {
+    if(isRacing) {
+        progressBar.style.width = "0%";
+    }
+}
+
+function barProgression() {
+    if(width >= 100) {
+        clearInterval(id);
+        isRacing = false;
+    } else {
+        width++;
+        progressBar.style.width = width + "%";
+    }
 }
